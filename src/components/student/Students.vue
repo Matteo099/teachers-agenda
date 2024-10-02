e5a6d11fa53b2880527c45e776d4f10b89a162db
Matteo Romagnoli <mromagnoli@sigmaspa.com>
2024-10-02 17:32:35 +0200

    feat: implemented edit / delete student

--- src/components/student/Students.vue
+++ src/components/student/Students.vue
@@ -1,7 +1,5 @@
 <template>
     <v-card elevation="3">
-        <!-- <p class="text-h5 text-center mb-6">Studenti della scuola {{ schoolName }}</p> -->
-
         <v-card-text>
             <v-row class="mr-4 mb-1 align-center">
                 <v-text-field class="ma-2" prepend-inner-icon="mdi-magnify" density="comfortable"
@@ -20,6 +18,28 @@
                 <template v-slot:item.lessonDay="{ item }">
                     {{ days[item.lessonDay] }}
                 </template>
+                <template v-slot:item.actions="{ item }">
+                    <v-dialog v-model="dialog2" fullscreen>
+                        <template v-slot:activator="{ props: activatorProps }">
+                            <v-icon class="me-2" size="small" v-bind="activatorProps">
+                                mdi-pencil
+                            </v-icon>
+                        </template>
+
+                        <StudentEditor edit :schoolId="schoolId" :initialStudent="item" @close="dialog2 = false"
+                            @save="dialog2 = false">
+                        </StudentEditor>
+                    </v-dialog>
+                    <DeleteDialog :name="item.name + ' ' + item.surname" objName="Studente"
+                        :onDelete="async () => await deleteStudent(item)">
+                        <template v-slot:activator="{ props: activatorProps }">
+                            <v-icon size="small" v-bind="activatorProps">
+                                mdi-delete
+                            </v-icon>
+                        </template>
+                    </DeleteDialog>
+
+                </template>
             </v-data-table>
         </v-card-text>
     </v-card>
@@ -30,9 +50,10 @@
 import { DatabaseRef, useDB } from '@/models/firestore-utils';
 import { days, type Student } from '@/models/model';
 import type { Unsubscribe } from 'firebase/database';
-import { onSnapshot } from 'firebase/firestore';
+import { deleteDoc, doc, onSnapshot } from 'firebase/firestore';
 import { onMounted, onUnmounted, ref, type Ref } from 'vue';
 import StudentEditor from './StudentEditor.vue';
+import DeleteDialog from '../DeleteDialog.vue';
 
 interface StudentsProps {
     schoolId: string
@@ -45,6 +66,7 @@ const subscriptions: Unsubscribe[] = [];
 const students: Ref<Student[]> = ref([]);
 const loadingStudents = ref(false);
 const dialog = ref(false);
+const dialog2 = ref(false);
 const studentHeaders: any = [
     {
         title: 'Nome',
@@ -58,11 +80,21 @@ const studentHeaders: any = [
     /// TODO: possibile problema!!! => uno studente può fare una lezione programmata più volte la stessa settimana? 
     // Esempio viene sia il lunedì che il venerdì. In tal caso bisogna gestire un'insieme di giorni e non un singolo giorno 
     { title: 'Giorno della Lezione', key: 'lessonDay' },
+    { title: 'Operazioni', key: 'actions', sortable: false },
 ];
 
 function onSaveStudent(student?: Student) {
     if (student)
         dialog.value = false;
+}
+
+async function deleteStudent(student?: Student): Promise<boolean> {
+    try {
+        await deleteDoc(doc(studentsRef, student?.id));
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 async function loadStudents() {
