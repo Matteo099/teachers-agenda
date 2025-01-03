23d5eb99a0b145834023057b3089cac33faff519
Matteo Romagnoli <matteo02.romagnoli@studenti.unicam.it>
2025-01-03 08:10:26 +0100

    build project using eager route

--- dist/assets/BackButton.vue_vue_type_script_setup_true_lang-C8dZMgDG.js
+++
@@ -1,1 +0,0 @@
-import{d as e,y as c,b as r,e as a,c as s}from"./index-DjUe38IL.js";const u=e({__name:"BackButton",setup(_){const o=c();function t(){o.go(-1)}return(p,i)=>{const n=r("v-btn");return a(),s(n,{icon:"mdi-arrow-left",onClick:t,variant:"text"})}}});export{u as _};
